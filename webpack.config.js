6bea7d5cadb9d2c676cdfdd2cbffdf2fecf573e1
Robert Beekman <robert@matsimitsu.nl>
2019-11-09 14:00:52 +0100

    Move to WebPack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: [
+    "./source/assets/javascripts/site.js",
+    "./source/assets/stylesheets/site.css.scss"
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, ".tmp", "dist"),
+    publicPath: "/"
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          "file-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          "file-loader",
+        ],
+      },
+      {
+        test: /font-awesome\.config\.js/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'font-awesome-loader' }
+        ]
+      },
+    ],
+  },
+};
