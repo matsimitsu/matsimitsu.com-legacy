a751ec5f9a7d1d9d3ae41f4af4ddf44de8a2cf48
Robert Beekman <robertbeekman@matsimitsu.nl>
2020-08-22 19:49:33 +0200

    Handle markdown with fontmatter

--- functions/micropub.js
+++ functions/micropub.js
@@ -4,11 +4,6 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN,
 })
 
-function valueOrDefault(object, path, defaultValue) {
-  if (!object[path]) { return defaultValue }
-  object[path][0] || defaultValue
-}
-
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
   if (!event.headers["authorization"] || event.headers["authorization"] != "Bearer " + process.env.TOKEN){
@@ -29,16 +24,20 @@ exports.handler = (event, context, callback) => {
   const data = JSON.parse(event.body)
   console.log("Data: ", data);
   const title = data["properties"]["name"][0]
+  const content = data["properties"]["content"][0]
   const date = new Date()
   const filename = [date.toISOString().split('T')[0], title.replace(/[\W]+/g,"-")].join("-")
-  const fileContent =
-   ['---',
-    'date: ' + date.toISOString(),
-    'title: ' + title,
-    'category: ' + valueOrDefault(data["properties"], "category", "note"),
-    '---',
-    data["properties"]["content"][0]
-   ].join('\n');
+  var fileContent = []
+
+  // If we've written a post without fontmatter, insert default forntmatter
+  if (!content.includes("---")) {
+    fileContent.push("---")
+    fileContent.push('date: ' + date.toISOString())
+    fileContent.push('title: ' + title)
+    fileContent.push('category: note')
+    fileContent.push('---')
+  }
+  fileContent.push(content)
 
   /* construct the fauna query */
   return octokit.repos.createOrUpdateFileContents({
@@ -46,7 +45,7 @@ exports.handler = (event, context, callback) => {
     repo: "matsimitsu.com",
     message: ("Adding note: " + title),
     path: "source/notes/" + filename + ".html.md",
-    content: Buffer.from(fileContent).toString("base64")
+    content: Buffer.from(fileContent.join("\n")).toString("base64")
   }).then((response) => {
     callback(null, {
       statusCode: 201,
