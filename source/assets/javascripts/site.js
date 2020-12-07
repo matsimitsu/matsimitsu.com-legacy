48fa7f2834cf56dd5eb9ee2c2570061ffeead43c
Robert Beekman <robertbeekman@matsimitsu.nl>
2020-12-07 20:35:47 +0100

    Add Turbolinks

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,15 +1,21 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Turbolinks from "turbolinks"
+
+Turbolinks.start()
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-document.addEventListener('DOMContentLoaded', function (event) {
+function setImageSizes() {
   const images = document.querySelectorAll("img")
   images.forEach((image) => {
     if (image.offsetWidth > 0) {
       image.setAttribute("sizes", `${image.offsetWidth}px`)
     }
   })
-})
+}
+
+document.addEventListener("turbolinks:load", setImageSizes)
+document.addEventListener('DOMContentLoaded', setImageSizes)
